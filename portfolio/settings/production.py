e7829be06c24e003bd93fa01b35cc2683d69d7ac
Gbgourav <gbgourav123@gmail.com>
2023-01-26 15:32:59 +0530

    update

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -46,7 +46,7 @@ DB_HOST = config("DB_HOST")
 DB_PORT = config("DB_PORT")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 CSRF_TRUSTED_ORIGINS = ['https://1-web-production.up.railway.app/']
 
