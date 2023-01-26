ae59209ec44ffd92564dfd879c6388274a15f3ba
Gbgourav <gbgourav123@gmail.com>
2023-01-26 16:26:13 +0530

    update

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -147,9 +147,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static').replace('\\', '/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'media/static')
 MEDIA_URL = '/media/'
-STATIC_URL = '/static/'
+STATIC_URL = 'media/static/'
+
 
 
 
