d36d90a9a006b656daa3c82e1a219b7f933ec653
Gbgourav <gbgourav123@gmail.com>
2023-01-26 13:29:46 +0530

    update

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -146,14 +146,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "gourav-portfolio-main/assets/")
-
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "gourav-portfolio-main", 'media')
-
-STATICSITEMAPS_ROOT_DIR = os.path.join(os.path.dirname(BASE_DIR), "gourav-portfolio-main/static/")
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
+# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "gourav-portfolio-main/assets/")
+#
+# MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "gourav-portfolio-main", 'media')
+#
+# STATICSITEMAPS_ROOT_DIR = os.path.join(os.path.dirname(BASE_DIR), "gourav-portfolio-main/static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
