6cd6f93ddcf40019922d54cf3ccb20415d47ef84
Gbgourav <gbgourav123@gmail.com>
2023-01-26 13:38:02 +0530

    update

--- portfolio/settings/__init__.py
+++ portfolio/settings/__init__.py
@@ -1,2 +1,1 @@
 from .base import *
-# from .production import *
--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -143,22 +143,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static').replace('\\', '/')
 MEDIA_URL = '/media/'
-
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
-# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "gourav-portfolio-main/assets/")
-#
-# MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "gourav-portfolio-main", 'media')
-#
-# STATICSITEMAPS_ROOT_DIR = os.path.join(os.path.dirname(BASE_DIR), "gourav-portfolio-main/static/")
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+STATIC_URL = '/static/'
 
 
 
