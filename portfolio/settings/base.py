7b284cc1cda04d2d60380a6fc95c0ee417d7b5b7
Gbgourav <gbgourav123@gmail.com>
2023-01-26 15:10:02 +0530

    update

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -139,6 +139,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+CSRF_TRUSTED_ORIGINS = ['https://1-web-production.up.railway.app/']
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
