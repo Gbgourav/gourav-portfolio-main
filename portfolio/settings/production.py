62c9ef790c1557839b2c0c46d4511433c25d3e0a
Gbgourav <gbgourav123@gmail.com>
2023-04-11 22:39:39 +0530

    update

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret
 SECRET_KEY = config('SECRET_KEY')
 
 EMAIL_HOST = 'smtp.mailtrap.io'
